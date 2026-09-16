ce648fa67b5a5b9b1575ed5d3db9f90b9e37699f
alinstan1 <alinwillwide@gmail.com>
2026-09-16 20:33:41 +0300

    Added book cover

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
+    import Cover from "./Cover.svelte";
   import Prompt from "./Prompt.svx";
   import Scroll from "./scroll/Scroll.svelte";
 </script>
 
+<Cover />
 <Prompt />
 <Scroll />
---
+++ src/Cover.svelte
@@ -0,0 +1,21 @@
+<div class="cover">
+	<img src="/cover.png" alt="IRLance book cover" />
+</div>
+
+<style>
+	.cover {
+		display: flex;
+		justify-content: center;
+		margin: 0 auto;
+	}
+
+	img {
+		border-radius: 0.5rem;
+		display: block;
+		aspect-ratio: auto;
+		max-width: 100%;
+		max-height: calc(100vh - 24px);
+		width: auto;
+		height: auto;
+	}
+</style>
